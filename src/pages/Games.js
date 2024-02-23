723fa352389867e49f1f8c2c5923f468b548a29b
Igor-Benez <103526457+Igor-Benez@users.noreply.github.com>
2024-02-22 23:47:58 -0300

    adicionando card

--- src/pages/Games.js
+++ src/pages/Games.js
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import CardGameDetails from "../components/CardGameDetails/CardGameDetails";
 import Button from "../components/Button";
+import Card from "../components/Cards/cards";
 
 export default function Games(props) {
   const { userInfo } = props;
@@ -42,42 +43,21 @@ export default function Games(props) {
         <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
           <table className="w-full text-sm text-left rtl:text-right text-gray-500">
             <thead className="w-full text-xs text-gray-700 uppercase bg-gray-50">
-              <tr>
-                <th className="px-6 py-3">Jogo</th>
-                <th className="px-6 py-3">Nome</th>
-                <th className="px-6 py-3">Categoria</th>
-                <th className="px-6 py-3">Avaliação geral</th>
-                <th className="px-6 py-3">Opções</th>
-              </tr>
+              
             </thead>
             <tbody>
               {game.map((game) => (
-                <tr className="bg-white border-b" key={game["_id"]}>
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    <img
-                      src={game.imageURL}
-                      width={60}
-                      alt={`Jogo ${game.name}`}
-                    ></img>
-                  </td>
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {game.name}
-                  </td>
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {game.category ? game.category.name : "Sem categoria"}
-                  </td>
-                  <td className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                    {game.score || "Sem avaliação"}
-                  </td>
-                  <td>
+                
                     <Link to={`/gameDetails/${game["_id"]}`}>
-                      <Button
-                        text="Visualizar detalhes"
-                        classCSS="btnGradient"
-                      />
-                    </Link>
-                  </td>
-                </tr>
+                      <Card 
+                      name = {game.name}
+                      category = {game.category.name}
+                      imageURL = {game.imageURL}
+                      url = {game.url} 
+                      score = {game.score}
+                      description = {game.description}
+                      />                      
+                    </Link>                  
               ))}
             </tbody>
           </table>
@@ -97,3 +77,5 @@ export default function Games(props) {
     </>
   );
 }
+
+
